b04ef308881a606f65adf30081f1017545934af1
Lucas Martins <martinslucas258@gmail.com>
2023-12-06 15:18:48 -0300

    new

--- Python - Mundo 1/Aulas Praticas/Aula-04.py
+++ Python - Mundo 1/Aulas Praticas/Aula-04.py
@@ -4,11 +4,11 @@ print(7+4)
 print('7'+'5')
 6+4
 nome = ('guanabara')
-idade= 25
+idade = 25
 peso = 78.8
-print(nome,idade,peso)
+print(nome, idade, peso)
 
 nome = input('Qual é seu nome? ')
-idade= input('Qual é sua idade? ')
+idade = input('Qual é sua idade? ')
 peso = input('Qual é seu peso? ')
-print(nome,idade,peso)
+print(nome, idade, peso)
